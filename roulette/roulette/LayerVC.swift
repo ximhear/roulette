cb6611489bd37048eb0351396f42164bde0d1302
chlee <chlee@gpmobile.co.kr>
2017-07-17 02:08:19 +0900

    layer에 정지 알고리즘 적용. 2차 함수 사용.

--- roulette/roulette/LayerVC.swift
+++ roulette/roulette/LayerVC.swift
@@ -28,15 +28,37 @@ class LayerVC: UIViewController {
     @IBAction func rotateClicked(_ sender: Any) {
         GZLogFunc()
         
-        let rotate = CABasicAnimation(keyPath: "transform.rotation.z")
-        let toValue = Double.pi * 21
-        rotate.duration = 2
-        rotate.fromValue = 0
-//        rotate.isRemovedOnCompletion = false
-        rotate.fillMode = kCAFillModeForwards
-        rotate.toValue = toValue
+//        let rotate = CABasicAnimation(keyPath: "transform.rotation.z")
+//        let toValue = Double.pi * 21
+//        rotate.duration = 10
+//        rotate.fromValue = 0
+////        rotate.isRemovedOnCompletion = false
+//        rotate.fillMode = kCAFillModeForwards
+//        rotate.toValue = toValue
+//
+//        rouletteView.layer.transform = CATransform3DMakeRotation(CGFloat(toValue), 0, 0, 1)
+//        self.rouletteView.layer.add(rotate, forKey: "")
         
-        rouletteView.layer.transform = CATransform3DMakeRotation(CGFloat(toValue), 0, 0, 1)
-        self.rouletteView.layer.add(rotate, forKey: "")
+        let rotate = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+        let duration: Int = 10
+        let fps : Int = 60
+        rotate.duration = CFTimeInterval(duration)
+        var keyTimes = [NSNumber]()
+        var values = [Double]()
+        let b: Double = Double.pi * 21 + Double.pi / 4
+        for x in 0...(duration * fps) {
+            let xx = Double(x) / Double(fps)
+            keyTimes.append(NSNumber(value: xx / Double(duration)))
+            values.append(-b / Double(duration*duration) * (xx - Double(duration) ) * (xx - Double(duration) ) + b)
+        }
+        rotate.keyTimes = keyTimes
+        rotate.values = values
+
+        GZLogFunc(rotate.keyTimes)
+        GZLogFunc(rotate.values)
+        rotate.fillMode = kCAFillModeForwards
+        rouletteView.layer.transform = CATransform3DMakeRotation(CGFloat(b), 0, 0, 1)
+        self.rouletteView.layer.add(rotate, forKey: nil)
+        
     }
 }
--- roulette/roulette/ViewVC.swift
+++ roulette/roulette/ViewVC.swift
@@ -30,17 +30,68 @@ class ViewVC: UIViewController {
     @IBAction func rotateClicked(_ sender: Any) {
         GZLogFunc()
         
-        UIView.animate(withDuration: 3, delay: 0, options: [UIViewAnimationOptions.curveLinear], animations: {
+        UIView.animate(withDuration: 3, delay: 0, options: [UIViewAnimationOptions.curveEaseOut], animations: {
             GZLogFunc()
-            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
-            self.imageView.transform = CGAffineTransform(rotationAngle: 0)
-            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
-            self.imageView.transform = CGAffineTransform(rotationAngle: 0)
-            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
-            self.imageView.transform = CGAffineTransform(rotationAngle: 0)
+            for _ in 0..<10 {
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+                self.imageView.transform = CGAffineTransform(rotationAngle: 0)
+            }
         }) { (completed) in
             GZLogFunc(completed)
         }
+//        UIView.animate(withDuration: 7.5, delay: 2.0, options: [UIViewAnimationOptions.curveEaseOut], animations: {
+//            GZLogFunc()
+//            for _ in 0..<1 {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: 0)
+//            }
+//            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/1))
+//        }) { (completed) in
+//            GZLogFunc(completed)
+//        }
+
+//        UIView.animateKeyframes(withDuration: 10, delay: 0, options: [.calculationModePaced], animations: {
+//            GZLogFunc()
+//
+//            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 3 / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 1.0, relativeDuration: 0.5, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 3 / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 1.5, relativeDuration: 1, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 2.5, relativeDuration: 1, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 3 / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 3.5, relativeDuration: 2, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 1))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 5.5, relativeDuration: 2, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 3 / 2))
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+//            })
+//            UIView.addKeyframe(withRelativeStartTime: 7.5, relativeDuration: 2.5, animations: {
+//                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 4))
+//            })
+//
+//        }) { (completed) in
+//            GZLogFunc(completed)
+//        }
     }
 
 }
